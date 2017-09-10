9eff3927b2b2168f2f371d9ec13cc1a0bc4c7ff5
MarcJacob <marcjacobpro@gmail.com>
2017-09-10 16:04:37 +0200

    Fix : Dossier debug

---
+++ Sources/Engine/Debug/ConsoleLine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BloodAndBileEngine
+{
+    public struct ConsoleLine
+    {
+        public string Text;
+        public Color TextColor;
+
+        public ConsoleLine(string txt, Color col)
+        {
+            Text = txt;
+            TextColor = col;
+        }
+    }
+}
