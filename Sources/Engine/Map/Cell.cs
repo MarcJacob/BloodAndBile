4232c1b0df7e0655bd212b1d61f7c85e17220604
Schenger <manu-26@live.fr>
2017-09-24 14:24:05 +0200

    Création cell + world. Ajout des fonctionnalités de base : constructeurs + attributs

---
+++ Sources/Engine/Map/Cell.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* 
+ * Cette classe contient l'enssemble des Entity qui doivent etre contenues sur cette cellule.
+ * Sera utilisé pour le pathfinding
+ */
+
+public class Cell
+{
+    List<Entity> Entities;
+    Vector3 Coordinates;
+
+    //Compléter lorsqu'Ilan aura terminé et transféré sa classe Entity
+
+    public Cell(float posX, float posY, float high)
+    {
+        posX *= World.SizeCellX;
+        posY *= World.SizeCellY; 
+        Coordinates = new Vector3(posY, high, posX);
+        Entities = new List<Entity>();
+    }
+
+
+
+}
