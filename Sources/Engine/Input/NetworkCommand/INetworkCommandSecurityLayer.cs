90b7753932c41c89b2faf9036de4a25f15f678a4
MarcJacob <marcjacobpro@gmail.com>
2017-11-30 18:56:35 +0100

    Ajout support couches de sécurité pour les commandes NetCommand

---
+++ Sources/Engine/Input/NetworkCommand/INetworkCommandSecurityLayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/// <summary>
+/// "Couche" de sécurité pour les commandes.
+/// Toute classe implémentant cette interface doit pouvoir dire s'il gère la sécurité pour une commande donnée,
+/// et dire si, d'après lui, une commande devrait pouvoir être exécutée ('laisser passer'), ou doit être
+/// bloquée là ('arrêter').
+/// </summary>
+namespace BloodAndBileEngine
+{
+    public interface INetworkCommandSecurityLayer
+    {
+        bool CanCheckCommand(string command);
+        bool DoSecurityCheck(string command, object[] args);
+    }
+}
---
+++ Sources/Engine/Input/NetworkCommand/NetworkCommandManager.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BloodAndBileEngine.Networking;
+
+namespace BloodAndBileEngine
+{
+    /// <summary>
+    /// Gestionnaire des commandes à envoyer à une autre machine
+    /// </summary>
+   public static class NetworkCommandManager
+    {
+        static void NetworkCommandHandler(object[] args)
+        {
+            if (args.Length > 2)
+            {
+                int idConnection;
+                if (args[0] is string)
+                {
+                    if (!Int32.TryParse((string)args[0], out idConnection))
+                    {
+                        Debugger.Log("Erreur conversion ID de connexion lors du TryParse", UnityEngine.Color.red);
+                        return;
+                    }
+                }
+                else if(args[0] is int)
+                {
+                    idConnection = (int)args[0];
+                }
+                else
+                {
+                    Debugger.Log("L'ID entré en argument n'est pas un int ni un string", UnityEngine.Color.red);
+                    return;
+                }
+                string command = (string)args[1];
+                string[] parameters = new string[args.Length - 2];
+                for (int i = 2; i < args.Length; i++)
+                {
+                    parameters[i - 2] = (string)args[i];
+                }
+                MessageSender.Send(new NetworkCommandMessage(command, parameters), idConnection);
+            }
+            else Debugger.Log("Trop peu d'arguments dans la commande 'NetCommand'", UnityEngine.Color.red);
+        }
+
+        public static void Initialize()
+        {
+            InputManager.AddHandler("NetCommand", NetworkCommandHandler);
+            MessageReader.AddHandler(60002, NetworkCommandReceiver);
+        }
+
+        static void NetworkCommandReceiver(NetworkMessageInfo info, NetworkMessage message)
+        {
+            NetworkCommandMessage commandMessage = (NetworkCommandMessage)message;
+            if(SecurityCheck(info.ConnectionID, commandMessage)) // Sécurité
+            {
+                // On a passé la sécurité ! Exécuter la commande.
+                if (commandMessage.Args == null || commandMessage.Args.Length == 0)
+                {
+                    InputManager.SendCommand(commandMessage.Command);
+                }
+                else
+                {
+                    InputManager.SendCommand(commandMessage.Command, commandMessage.Args);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Couches de sécurité.
+        /// Note : les couches de sécurité seront, la plupart du temps, définis dans le code des sous-projets Unity.
+        /// </summary>
+        static List<INetworkCommandSecurityLayer> SecurityLayers = new List<INetworkCommandSecurityLayer>()
+        {
+
+        };
+
+        static public void AddSecurityLayer(INetworkCommandSecurityLayer layer)
+        {
+            bool contains = false;
+            int i = 0;
+            while (!contains && ++i < SecurityLayers.Count)
+            {
+                if (SecurityLayers[i].GetType() == layer.GetType()) // On évite d'avoir deux couches de sécurité du même type.
+                {
+                    contains = true;
+                }
+            }
+
+            if (!contains)
+            {
+                SecurityLayers.Add(layer);
+            }
+        }
+
+        static public void RemoveSecurityLayer<T>() where T : INetworkCommandSecurityLayer
+        {
+            int i = 0;
+            bool removed = false;
+            while (!removed && ++i < SecurityLayers.Count)
+            {
+                if (SecurityLayers[i].GetType() == typeof(T))
+                {
+                    removed = true;
+                    SecurityLayers.RemoveAt(i);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Itère à travers toutes les couches de sécurité savant gérer la commande passée en paramètres.
+        /// </summary>
+        /// <returns></returns>
+        static bool SecurityCheck(int connectionID, NetworkCommandMessage commandMessage)
+        {
+            bool passed = true; // Par défaut à true au cas où il n'y aurait aucune couche ne supportant cette commande.
+            int i = 0;
+            while (++i < SecurityLayers.Count && passed)
+            {
+                if (SecurityLayers[i].CanCheckCommand(commandMessage.Command))
+                {
+                    passed = SecurityLayers[i].DoSecurityCheck(commandMessage.Command, commandMessage.Args);
+                }
+            }
+
+            return passed;
+        }
+    }
+}
